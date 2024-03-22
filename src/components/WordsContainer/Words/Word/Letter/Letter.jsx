8b54d26497a21b2ed7892e412efbd15fe3a911d6
Aldo Aliscioni <aldoaliscioni18@gmail.com>
2024-03-22 00:36:59 -0300

    refactor code

--- src/components/WordsContainer/Words/Word/Letter/Letter.jsx
+++ src/components/WordsContainer/Words/Word/Letter/Letter.jsx
@@ -1,23 +1,25 @@
-import { gameStore } from "@store/store";
+import { useMemo } from "react"
+import { gameStore } from "@store/store"
 import style from "./Letter.module.css"
 
 const Letter = ({ letter, typed, indexWord, indexLetter, actualLetter, word, fullTyped, incorrect }) => {
-  const { steps } = gameStore((state) => state);
+  const { steps } = gameStore((state) => state)
 
-  const correctClasses = [];
-  correctClasses.push(style.letter);
-  if (letter === typed) correctClasses.push(style.correct);
-  if ((letter !== typed && typed) || incorrect) correctClasses.push(style.incorrect);
+  const activeWordIndex = actualLetter?.wordIndex - steps.start
+  const isCurrentLetter = indexWord === activeWordIndex && indexLetter === actualLetter?.letterIndex
+  const isLastLetter = indexWord === activeWordIndex && fullTyped.length === word?.length && indexLetter + 1 >= word?.length
+  const isLastIncorrect = incorrect && fullTyped.length === indexLetter + 1 && indexWord === activeWordIndex
 
-  if (indexWord === actualLetter?.wordIndex - steps.start && indexLetter === actualLetter?.letterIndex) {
-    correctClasses.push(style.active);
-  }
-  else if ((indexWord === actualLetter?.wordIndex - steps.start && fullTyped.length === word?.length && indexLetter + 1 >= word?.length)
-    || (incorrect && fullTyped.length === indexLetter + 1 && indexWord === actualLetter?.wordIndex - steps.start)) {
-    correctClasses.push(style.final);
-  }
+  const correctClasses = useMemo(() => {
+    const classes = [style.letter]
+    if (letter === typed) classes.push(style.correct)
+    if ((letter !== typed && typed) || (incorrect)) classes.push(style.incorrect)
+    if (isCurrentLetter) classes.push(style.active)
+    else if (isLastLetter || isLastIncorrect) classes.push(style.final)
+    return classes.join(" ")
+  }, []);
 
-  return <span key={crypto.randomUUID()} className={correctClasses.join(" ")}>{letter}</span>
-}
+  return <span key={crypto.randomUUID()} className={correctClasses}>{letter}</span>
+};
 
-export default Letter
+export default Letter;
